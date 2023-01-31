e1529f977ec6066cc9ff6cea5bc86f3a32366ea9
Javohir Jumanov <jumanovjavohir49@gmail.com>
2023-01-31 17:30:24 +0500

    write some Subject services :)

---
+++ BulungurAcademy.Application/BulungurAcademy.Application/Services/Subjects/ISubjectServices.cs
@@ -0,0 +1,11 @@
+﻿using BulungurAcademy.Application.DataTransferObjects;
+
+namespace BulungurAcademy.Application.Services;
+public interface ISubjectServices
+{
+    ValueTask<SubjectDto> CreateSubjectAsync(SubjectForCreationDto subjectForCreationDto);
+    // IQueryable<SubjectDto> RetrieveSubjects(QueryParameter queryParameter);
+    ValueTask<SubjectDto> RetrieveSubjectByIdAsync(Guid userId);
+    ValueTask<SubjectDto> ModifySubjectAsync(SubjectForModificationDto subjectForModificationDto);
+    ValueTask<SubjectDto> RemoveSubjectAsync(Guid userId);
+}
---
+++ BulungurAcademy.Application/BulungurAcademy.Application/Services/Subjects/SubjectServices.cs
@@ -0,0 +1,43 @@
+﻿using BulungurAcademy.Application.DataTransferObjects;
+using BulungurAcademy.Infrastructure.Repositories.Subjects;
+
+namespace BulungurAcademy.Application.Services;
+public partial class SubjectServices : ISubjectServices
+{
+    //============= partial bo'ldi keyinchalik validatsiyalarni o'rnatish va boshqa ammallar uchun
+    private readonly ISubjectRepository subjectRepository;
+    public SubjectServices(ISubjectRepository subjectRepository) =>
+        this.subjectRepository = subjectRepository;
+
+    /// <summary>
+    /// Subject Create
+    /// </summary>
+    public ValueTask<SubjectDto> CreateSubjectAsync(SubjectForCreationDto subjectForCreationDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Get subject Retrieve by id
+    /// </summary>
+    public ValueTask<SubjectDto> RetrieveSubjectByIdAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Subject Modify
+    /// </summary>
+    public ValueTask<SubjectDto> ModifySubjectAsync(SubjectForModificationDto subjectForModificationDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Subject Remove
+    /// </summary>
+    public ValueTask<SubjectDto> RemoveSubjectAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+}
